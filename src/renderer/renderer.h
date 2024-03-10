d10edd8ee1b27acd4af7da01da597673a7353ade
trueGmek <gmek0204@gmail.com>
2024-03-10 20:51:28 +0100

    Changed Cmake build dir to local build directory

--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -2,7 +2,6 @@
 #include "GUI/GUI.h"
 #include "shader.h"
 #include <GLFW/glfw3.h>
-#include <cstddef>
 #include <memory>
 
 class Renderer {
@@ -25,8 +24,8 @@ private:
   void ProcessEvents();
   bool CreateWindow();
 
-  const std::string VERTEX_SHADER_PATH = "./resources/shaders/triangle.vert";
-  const std::string FRAG_SHADER_PATH = "./resources/shaders/triangle.frag";
+  const std::string VERTEX_SHADER_PATH = "../resources/shaders/triangle.vert";
+  const std::string FRAG_SHADER_PATH = "../resources/shaders/triangle.frag";
 
   const float vertices[9] = {
       -0.5f, -0.5f, 0.0f, //
